eeff3d47b32b2192379a8117647a0f4eeb34c469
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-11-21 17:17:39 -0600

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -9,17 +9,22 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-#Provider profile and region in which all the resources will create
+# Provider profile and region in which all the resources will create
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
 
-#Resource to create s3 bucket
+# Resource to create s3 bucket
 resource "aws_s3_bucket" "demo-bucket"{
   bucket = "ck-demo-bucket"
 
   tags = {
     Name = "S3Bucket"
   }
+}
+
+# Resource to create sns topic
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
 }
